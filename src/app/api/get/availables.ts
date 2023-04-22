41616f44d69836705e97d0f04b2289fcf19b2f83
WERDXZ <lwerdxzl@hotmail.com>
2023-04-22 09:27:20 -0700

    remove not needed file

--- src/app/api/get/availables.ts
+++
@@ -1,9 +0,0 @@
-export default function available
-(start: number, 
-	size: number, 
-	order: "time-asc" 
-	| "time-des" 
-	| "pop-asc" 
-	| "pop-des"): string[] {
-	return ["it is the session 1", "it is the session two", "..."]
-}
