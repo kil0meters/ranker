aa7b4dcc0cb73e75d3bd099b50cca97e15ad37c9
kilometers <kilo@meters.sh>
2023-04-22 14:59:58 -0700

    ordering and uh i don't rmember let me sleep ket me sleep hahah please let me sleep

--- src/app/rank/[id]/elo-ranking.tsx
+++ src/app/rank/[id]/elo-ranking.tsx
@@ -7,16 +7,22 @@ export async function EloRanking({ items }: { items: EloRankingItem[] }) {
     return (
         <table className="w-full">
             <thead>
-                <th className="border border-neutral-300 bg-neutral-50">Item</th>
+                <tr>
+                    <th className="border border-neutral-300 bg-neutral-50 p-2">#</th>
+                    <th className="border border-neutral-300 bg-neutral-50 p-2">Item</th>
+                    <th className="border border-neutral-300 bg-neutral-50 p-2">Elo</th>
+                </tr>
             </thead>
 
-            {items.map((item, i) =>
-                <tr className="group" key={i}>
-                    <td className="group-last-of-type:rounded-bl border border-neutral-300 p-2">{i + 1}</td>
-                    <td className="border border-neutral-300 p-2">{item.name}</td>
-                    <td className="group-last-of-type:rounded-br border border-neutral-300 p-2">{item.elo}</td>
-                </tr>
-            )}
+            <tbody>
+                {items.map((item, i) =>
+                    <tr className="group" key={i}>
+                        <td className="group-last-of-type:rounded-bl border border-neutral-300 p-2">{i + 1}</td>
+                        <td className="border border-neutral-300 p-2">{item.name}</td>
+                        <td className="group-last-of-type:rounded-br border border-neutral-300 p-2">{item.elo}</td>
+                    </tr>
+                )}
+            </tbody>
         </table>
     );
 }
--- src/app/rank/[id]/page.tsx
+++ src/app/rank/[id]/page.tsx
@@ -1,50 +1,118 @@
 import { prisma } from "@/dbconfig";
 import { notFound } from "next/navigation";
-import { GuessButton } from "./guess-button";
 import { EloRanking } from "./elo-ranking";
 import { getServerSession } from "next-auth/next";
 import { RankingItem } from "@prisma/client";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getPairByIndex } from "@/order";
+import { GuessButtonContainer } from "./guess-button";
 
 async function LocalLeaderboard({ rankingId }: { rankingId: string }) {
-	const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
-	if (!session || !session.user) {
-		return (
-			<div className="bg-neutral-300 h-72 rounded flex">
-				<span className="mx-auto my-auto">
-					You need to log in
-				</span>
-			</div>
-		);
-	} else {
-		const ranking = await prisma.userRankingItemElo.findMany({
-			select: {
-				elo: true,
-				rankingItem: true
-			},
-			where: {
-				userId: session.user.id,
-				rankingItem: {
-					rankingId
-				}
-			},
-			orderBy: {
-				elo: "desc"
-			}
-		});
+    if (!session || !session.user) {
+        return (
+            <div className="bg-neutral-300 h-72 rounded flex">
+                <span className="mx-auto my-auto">
+                    You need to log in
+                </span>
+            </div>
+        );
+    } else {
+        const ranking = await prisma.userRankingItemElo.findMany({
+            select: {
+                elo: true,
+                rankingItem: true
+            },
+            where: {
+                userId: session.user.id,
+                rankingItem: {
+                    rankingId
+                }
+            },
+            orderBy: {
+                elo: "desc"
+            }
+        });
 
-		return (
-			<EloRanking items={
-				ranking.map(item => {
-					return { elo: item.elo, name: item.rankingItem.text }
-				})
-			} />
-		);
-	}
+        return (
+            <EloRanking items={
+                ranking.map(item => {
+                    return { elo: item.elo, name: item.rankingItem.text }
+                })
+            } />
+        );
+    }
+}
+
+async function GuessButtons({ rankingId }: { rankingId: string }) {
+    let index: number;
+
+    const session = await getServerSession(authOptions);
+
+    if (session) {
+        let v = await prisma.userRankingItemChoiceIndex.upsert({
+            where: {
+                userId_rankingId: {
+                    userId: session.user.id,
+                    rankingId,
+                }
+            },
+            update: {},
+            create: {
+                userId: session.user.id,
+                rankingId,
+                index: 0,
+            }
+        });
+
+        index = v.index;
+    } else {
+        index = 0;
+    }
+
+    let count = await prisma.rankingItem.count({
+        where: {
+            rankingId
+        }
+    });
+
+    try {
+        const pair = getPairByIndex(count, index);
+
+        let options = await prisma.$transaction([
+            prisma.rankingItem.findFirst({
+                skip: pair[0],
+                orderBy: {
+                    id: "desc",
+                }
+            }),
+
+            prisma.rankingItem.findFirst({
+                skip: pair[1],
+                orderBy: {
+                    id: "desc",
+                }
+            })
+        ]) as unknown as RankingItem[];
+
+        if (!options[0] || !options[1]) return null;
+
+        return (
+            <GuessButtonContainer options={options} rankingId={rankingId} />
+        );
+    } catch (e) {
+        return (
+            <div className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 max-w-lg rounded-lg mx-auto p-8 shadow-md">
+                <span className="text-6xl font-bolder">You've rated everything.</span>
+            </div>
+        );
+    }
 }
 
 export default async function Ranking({ params }: { params: { id: string } }) {
+    const session = await getServerSession(authOptions);
+
     const ranking = await prisma.ranking.findUnique({
         select: {
             name: true,
@@ -63,47 +131,38 @@ export default async function Ranking({ params }: { params: { id: string } }) {
 
     if (!ranking) notFound();
 
-    const options: RankingItem[] = await prisma.$queryRaw`SELECT * FROM RankingItem WHERE rankingId = ${params.id} ORDER BY RAND() LIMIT 2`;
+    // const options: RankingItem[] = await prisma.$queryRaw`SELECT * FROM RankingItem WHERE rankingId = ${params.id} ORDER BY RAND() LIMIT 2`;
 
-	return (
-		<div className='mx-auto container flex flex-col gap-4'>
-			<aside className="w-auto w-max:10rem bg-blue-500 fixed top-16 right-2 mt-4">
-				<div>
-					<h1 className='text-2xl font-extrabold'>
-						{ranking.name}
-					</h1>
-					<span className='text-neutral-500'>
-						{ranking.user.name}
-					</span>
-				</div>
-				<EloRanking items={
-					ranking.RankingItem.map(item => {
-						return { elo: item.globalElo, name: item.text }
-					})
-				} />
-        
-			</aside>
+    return (
+        <div className='mx-auto container flex flex-col gap-4'>
+            <div>
+                <h1 className='text-2xl font-extrabold'>
+                    {ranking.name}
+                </h1>
+                <span className='text-neutral-500'>
+                    {ranking.user.name}
+                </span>
+            </div>
 
-			<div className="mr-40">
-				<div>
-					<h1 className='text-2xl font-extrabold'>
-						{ranking.name}
-					</h1>
-					<span className='text-neutral-500'>
-						{ranking.user.name}
-					</span>
-				</div>
+            <p>
+                {ranking.description}
+            </p>
 
-				<div className="grid grid-cols-2 w-full gap-8 mb-4">
-					<GuessButton rankingId={params.id} options={options} index={0} />
-					<GuessButton rankingId={params.id} options={options} index={1} />
-				</div>
-				<p>
-					{ranking.description}
-				</p>
-				<LocalLeaderboard rankingId={params.id} />
-			</div>
+            <GuessButtons rankingId={params.id} />
 
-		</div>
-	);
+            <div className="grid grid-cols-10 w-full gap-4">
+                <div className="col-start-3 col-end-6">
+                    <LocalLeaderboard rankingId={params.id} />
+                </div>
+
+                <div className="col-start-6 col-end-9">
+                    <EloRanking items={
+                        ranking.RankingItem.map(item => {
+                            return { elo: item.globalElo, name: item.text }
+                        })
+                    } />
+                </div>
+            </div>
+        </div>
+    );
 }
