b71d3c9408b56fc23772c971eed7a3b34e9554c8
WERDXZ <lwerdxzl@hotmail.com>
2023-04-22 01:58:53 -0700

    writing the new components!!!

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,43 +1,46 @@
-"use-client";
+"use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-type ApiResponse = {
-  title: string;
-  desc: string;
-  numberItem: number;
-  date: string;
-  url: string;
-}[];
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 
-async function callApi(path: string, body:JSON ): Promise<ApiResponse> {
-	const response = await fetch(path, {
-		method: "POST",
-		body: JSON.stringify(body),
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
 
-	return response.json();
-}
+// type ApiResponse = {
+//   title: string;
+//   desc: string;
+//   numberItem: number;
+//   date: string;
+//   url: string;
+// }[];
+//
+//
+// async function callApi(path: string, body:JSON ): Promise<ApiResponse> {
+// 	const response = await fetch(path, {
+// 		method: "POST",
+// 		body: JSON.stringify(body),
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 		},
+// 	});
+//
+// 	return response.json();
+// }
 
 const loadSize = 10;
 
 export async function showEntries() {
-	const entries = await callApi("/api/entries", JSON.parse(JSON.stringify({ "numload":loadSize })));
+	const entries = await prisma.ranking.findMany();
+
 
 	return (
 		<div className='grid grid-cols-1 grid-flow-row'>
 			{entries.map((entry, index) => (
 				<div className='border-b border-neutral-300 p-4 w-full mb-4' key={index}>
 					<div className='font-bold text-lg hover:underline'>
-						{entry.title}
+						{entry.name}
 					</div>
 					<div className='text-sm text-neutral-500'>
-						{entry.desc}
+						{entry.description}
 					</div>
 				</div>
 			))}
