ee42021563a0766d9c218c3922301189314dc28c
WERDXZ <lwerdxzl@hotmail.com>
2023-04-22 03:08:12 -0700

    dynamicccccccccccccc router for rankings !!!!!!!!

--- src/components/entries.tsx
+++ src/components/entries.tsx
@@ -9,9 +9,9 @@ export async function ShowEntries() {
 		<div className='grid grid-cols-1 grid-flow-row'>
 			{entries.map((entry, index) => (
 				<div className='border-b border-neutral-300 p-4 w-full mb-4' key={index}>
-					<div className='font-bold text-lg hover:underline'>
+					<a className='font-bold text-lg hover:underline' href={"/rank/"+entry.id}>
 						{entry.name}
-					</div>
+					</a>
 					<div className='text-sm text-neutral-500'>
 						{entry.description}
 					</div>
