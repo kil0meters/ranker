8a79bd9aad44b46da4d979a82c48ebed094d1801
WERDXZ <lwerdxzl@hotmail.com>
2023-04-21 23:53:24 -0700

    add api function for query, need implement

---
+++ src/app/api/get/info.ts
@@ -0,0 +1,12 @@
+
+export default function get(): {title:string,desc:string,numberItem:number,date:string,url:string} {
+
+	return{
+		title: "what ever the title is",
+		desc: "just a description",
+		numberItem: 0,
+		date: "April 1st, 2025",
+		url: "to the url"
+	}
+
+}
