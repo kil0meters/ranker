229f1a2f43b533c287000d0cf867b79d5cbd077e
WERDXZ <lwerdxzl@hotmail.com>
2023-04-22 14:36:49 -0700

    add blur effect to buttons when not availiable

--- src/app/rank/[id]/guess-button.tsx
+++ src/app/rank/[id]/guess-button.tsx
@@ -8,6 +8,12 @@ export function GuessButton({ rankingId, options, index }: { rankingId: string, 
     const choices = options.map(x => x.id);
 
     const click = async () => {
+		// blur all the button and disable after clicked util after fetched
+		const buttons = document.getElementsByClassName('needToToggle');
+		for (let i = 0; i < buttons.length; i++) {
+			buttons[i].classList.add('opacity-50', 'pointer-events-none');
+		}
+
         await fetch("/api/vote", {
             method: "POST",
             body: JSON.stringify({
@@ -18,11 +24,15 @@ export function GuessButton({ rankingId, options, index }: { rankingId: string, 
         });
 
         router.refresh();
+		for (let i = 0; i < buttons.length; i++) {
+			buttons[i].classList.remove('opacity-50', 'pointer-events-none');
+		}
+
     };
 
     return (
         <button
-            className='shadow-lg aspect-video p-8 text-3xl font-extrabold rounded-md transition-all bg-neutral-700 text-neutral-50 hover:bg-neutral-600 hover:shadow-xl'
+            className='needToToggle shadow-lg aspect-video p-8 text-3xl font-extrabold rounded-md transition-all bg-neutral-700 text-neutral-50 hover:bg-neutral-600 hover:shadow-xl'
             onClick={click}>
             {options[index].text}
         </button>
