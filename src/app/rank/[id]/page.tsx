67215ca453cd3005830f4a9788846a0e88c2857f
WERDXZ <lwerdxzl@hotmail.com>
2023-04-22 11:33:30 -0700

    merged with main

--- src/app/rank/[id]/page.tsx
+++ src/app/rank/[id]/page.tsx
@@ -17,7 +17,7 @@ async function LocalLeaderboard({ rankingId }: { rankingId: string }) {
 				You need to log in
 			</div>
 		);
-
+	const session = await getServerSession(authOptions);
     if (!session || !session.user) {
         return (
             <div className="bg-neutral-300">
@@ -36,17 +36,20 @@ async function LocalLeaderboard({ rankingId }: { rankingId: string }) {
                 rankingItem: {
                     rankingId
                 }
+            },
+            orderBy: {
+                elo: "desc"
             }
         });
 
-		return (
-			<EloRanking items={
-				ranking.map(item => {
-					return { elo: item.elo, name: item.rankingItem.text }
-				})
-			} />
-		);
-	}
+        return (
+            <EloRanking items={
+                ranking.map(item => {
+                    return { elo: item.elo, name: item.rankingItem.text }
+                })
+            } />
+        );
+    }
 }
 
 export default async function Ranking({ params }: { params: { id: string } }) {
@@ -56,66 +59,57 @@ export default async function Ranking({ params }: { params: { id: string } }) {
             name: true,
             description: true,
             user: true,
-            RankingItem: true
+            RankingItem: {
+                orderBy: {
+                    globalElo: "desc"
+                }
+            }
         },
         where: {
             id: params.id
         }
     });
 
-    const session = await getServerSession(authOptions);
-    console.log(session);
+	const session = await getServerSession(authOptions);
+	console.log(session);
 
-    if (!ranking) notFound();
-
-    const options: RankingItem[] = await prisma.$queryRaw`SELECT * FROM RankingItem WHERE rankingId = ${params.id} ORDER BY RAND() LIMIT 2`;
+	if (!ranking) notFound();
+	const options: RankingItem[] = await prisma.$queryRaw`SELECT * FROM RankingItem WHERE rankingId = ${params.id} ORDER BY RAND() LIMIT 2`;
 
 	return (
 		<div className='mx-auto container flex flex-col gap-4'>
 			<aside className="w-auto w-max:10rem bg-blue-500 fixed top-16 right-2 mt-4">
 
-						<EloRanking items={
-							ranking.RankingItem.map(item => {
-								return { elo: item.globalElo, name: item.text }
-							})
-						} />
+
+				<EloRanking items={
+					ranking.RankingItem.map(item => {
+						return { elo: item.globalElo, name: item.text }
+					})
+				} />
 
 			</aside>
 
-            <div className="grid grid-cols-2 w-full gap-8 mb-4">
-                <GuessButton rankingId={params.id} options={options} index={0} />
-                <GuessButton rankingId={params.id} options={options} index={1} />
-            </div>
+			<div className="mr-40">
+				<div>
+					<h1 className='text-2xl font-extrabold'>
+						{ranking.name}
+					</h1>
+					<span className='text-neutral-500'>
+						{ranking.user.name}
+					</span>
+				</div>
+
+				<div className="grid grid-cols-2 w-full gap-8 mb-4">
+					<GuessButton rankingId={params.id} options={options} index={0} />
+					<GuessButton rankingId={params.id} options={options} index={1} />
+				</div>
 
 				<p>
 					{ranking.description}
 				</p>
 
-				<div className="grid grid-cols-2 w-full gap-8 mb-4">
-					<GuessButton index={0} text={"C++"} />
-					<GuessButton index={1} text={"Rust"} />
-				</div>
-
-				<div className="grid grid-cols-10 gap-4">
-					<div className="col-start-3 col-end-6">
-						<h2 className="text-lg font-bold">Personal Leaderboard</h2>
-
-						<LocalLeaderboard rankingId={params.id} />
-					</div>
-
-					<div className="col-start-6 col-end-9">
-						<h2 className="text-lg font-bold">Global Leaderboard</h2>
-
-						<EloRanking items={
-							ranking.RankingItem.map(item => {
-								return { elo: item.globalElo, name: item.text }
-							})
-						} />
-					</div>
-				</div>
-
-
 			</div>
 		</div>
 	);
+	}
 }
