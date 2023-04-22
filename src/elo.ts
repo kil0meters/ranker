96d425e254895de73847e511d1bd9a1d2b85d452
mcsharland <mcsharland@mail.csuchico.edu>
2023-04-22 10:39:24 -0700

    Typehints for elo.ts

--- src/elo.ts
+++ src/elo.ts
@@ -1,10 +1,10 @@
 //helper function
-function getExpectedScore(player1, player2) {
+function getExpectedScore(player1: number, player2: number): number {
     return 1 / (1 + Math.pow(10, (player2 - player1) / 400));
 }
 
 //helper function
-function getRatingChange(player1, player2, outcome, kValue) {
+function getRatingChange(player1: number, player2: number, outcome: boolean, kValue: number): number {
     // const kValue = 50 //Scales the elo gains/losses
     const expectedScore = getExpectedScore(player1, player2);
     const actualScore = outcome ? 1 : 0;
@@ -12,16 +12,15 @@ function getRatingChange(player1, player2, outcome, kValue) {
     return Math.round(ratingChange);
 }
 
-/*
-player1Elo int
-player2Elo int
-outcome boolean
-player1kValue float
-player2kValue float
+//player1kValue & player2kValue optional for friendly (no value) & global
+function updateRatings(
+    player1Elo: number, 
+    player2Elo: number,
+    outcome: boolean,
+    player1kValue: number = 50,
+    player2kValue: number = 50
+    ): [number, number] {
 
-player1kValue & player2kValue optional for friendly (no value) & global
-*/
-function updateRatings(player1Elo, player2Elo, outcome, player1kValue = 50, player2kValue = 50) {
     const player1RatingChange = getRatingChange(player1Elo, player2Elo, outcome, player1kValue);
     const player2RatingChange = getRatingChange(player2Elo, player1Elo, !outcome, player2kValue);
 
