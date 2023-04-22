dee44ea37cf4e0386e5ed1edcbedb4031b97fc1b
WERDXZ <lwerdxzl@hotmail.com>
2023-04-22 11:26:01 -0700

    temp commit to sync with main

--- src/app/rank/[id]/elo-ranking.tsx
+++ src/app/rank/[id]/elo-ranking.tsx
@@ -9,6 +9,7 @@ export async function EloRanking({ items }: { items: EloRankingItem[] }) {
             <thead>
                 <th className="border border-neutral-300 bg-neutral-50">Item</th>
             </thead>
+
             {items.map((item, i) =>
                 <tr className="group" key={i}>
                     <td className="group-last-of-type:rounded-bl border border-neutral-300 p-2">{i + 1}</td>
--- src/app/rank/[id]/page.tsx
+++ src/app/rank/[id]/page.tsx
@@ -7,34 +7,32 @@ import { RankingItem } from "@prisma/client";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 async function LocalLeaderboard({ rankingId }: { rankingId: string }) {
-    const session = await getServerSession(authOptions);
+	const session = await getServerSession(authOptions);
 
-    if (!session || !session.user) {
-        return (
-            <div className="bg-neutral-300 h-72 rounded flex">
-                <span className="mx-auto my-auto">
-                    You need to log in
-                </span>
-            </div>
-        );
-
-    } else {
-        const ranking = await prisma.userRankingItemElo.findMany({
-            select: {
-                elo: true,
-                rankingItem: true
-            },
-            where: {
-                userId: session.user.id,
-                rankingItem: {
-                    rankingId
-                }
-            },
-            orderBy: {
-                elo: "desc"
-            }
-        });
-
+	if (!session || !session.user) {
+		return (
+			<div className="bg-neutral-300 h-72 rounded flex">
+				<span className="mx-auto my-auto">
+					You need to log in
+				</span>
+			</div>
+		);
+	} else {
+		const ranking = await prisma.userRankingItemElo.findMany({
+			select: {
+				elo: true,
+				rankingItem: true
+			},
+			where: {
+				userId: session.user.id,
+				rankingItem: {
+					rankingId
+				}
+			},
+			orderBy: {
+				elo: "desc"
+			}
+		});
 
 		return (
 			<EloRanking items={
@@ -47,24 +45,21 @@ async function LocalLeaderboard({ rankingId }: { rankingId: string }) {
 }
 
 export default async function Ranking({ params }: { params: { id: string } }) {
-    const ranking = await prisma.ranking.findUnique({
-        select: {
-            name: true,
-            description: true,
-            user: true,
-            RankingItem: {
-                orderBy: {
-                    globalElo: "desc"
-                }
-            }
-        },
-        where: {
-            id: params.id
-        }
-    });
-
-	const session = await getServerSession(authOptions);
-	console.log(session);
+	const ranking = await prisma.ranking.findUnique({
+		select: {
+			name: true,
+			description: true,
+			user: true,
+			RankingItem: {
+				orderBy: {
+					globalElo: "desc"
+				}
+			}
+		},
+		where: {
+			id: params.id
+		}
+	});
 
 	if (!ranking) notFound();
 	const options: RankingItem[] = await prisma.$queryRaw`SELECT * FROM RankingItem WHERE rankingId = ${params.id} ORDER BY RAND() LIMIT 2`;
@@ -72,7 +67,14 @@ export default async function Ranking({ params }: { params: { id: string } }) {
 	return (
 		<div className='mx-auto container flex flex-col gap-4'>
 			<aside className="w-auto w-max:10rem bg-blue-500 fixed top-16 right-2 mt-4">
-
+				<div>
+					<h1 className='text-2xl font-extrabold'>
+						{ranking.name}
+					</h1>
+					<span className='text-neutral-500'>
+						{ranking.user.name}
+					</span>
+				</div>
 				<EloRanking items={
 					ranking.RankingItem.map(item => {
 						return { elo: item.globalElo, name: item.text }
@@ -95,12 +97,12 @@ export default async function Ranking({ params }: { params: { id: string } }) {
 					<GuessButton rankingId={params.id} options={options} index={0} />
 					<GuessButton rankingId={params.id} options={options} index={1} />
 				</div>
-
 				<p>
 					{ranking.description}
 				</p>
-
+				<LocalLeaderboard rankingId={params.id} />
 			</div>
+
 		</div>
 	);
 }
